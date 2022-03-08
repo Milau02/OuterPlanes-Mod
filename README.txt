48d4e66575c3e027f52c1b8e0a37e6f0e21104e7
Milau02 <michaellaufer2@gmail.com>
2022-03-08 01:49:32 -0500

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,20 @@
+Ported:
+
+To Port:
+  - Garnet Gem / Garnet Ore
+  - Soulstone
+  - Soul Gate
+
+  - Astral Logs / Wood (With stripped variants)
+  - Astral Planks
+  - Astral Slab
+  - Astral Stairs
+  - Astral Door
+  - Astral Trees / Saplings / Leaves / Roots
+
+  - Soultree Logs / Wood (With stripped variants)
+  - Soultree Planks
+  - Soultree Slab
+  - Soultree Stairs
+  - Soultree Door
+  - Soultree Trees / Saplings / Leaves / Roots
---
+++ src/main/java/ml/milau/outerplanes/OuterPlanes.java
@@ -0,0 +1,43 @@
+package ml.milau.outerplanes;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(OuterPlanes.MOD_ID)
+public class OuterPlanes {
+    public static final String MOD_ID = "outerplanes";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public OuterPlanes() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the setup method for modloading
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
